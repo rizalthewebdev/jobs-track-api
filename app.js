ff31737ae1715644740fd15e3f12bf37795b95e1
rzlTrimelty <rzl.trimelty@gmail.com>
2022-03-09 21:15:31 +0700

    Delete Swagger API docs

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
-// Swagger
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yamljs");
-const swaggerDocument = YAML.load("./swagger.yaml");
-
 // Import function to connect with MongoDB
 const connectDB = require("./db/connect");
 
@@ -47,11 +42,8 @@ app.use(xss());
 
 // Home Route
 app.get("/", (req, res) =>
-   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+   res.send('<h1>Jobs API</h1>')
 );
-
-// Documentation Route Using Swagger
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // Route to authenntication
 app.use("/api/v1/auth", authRoutes);
