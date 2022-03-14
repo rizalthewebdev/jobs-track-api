6b1b1e9f2698c501b2dcdb6a3103563ad1a2574e
rzlTrimelty <rzl.trimelty@gmail.com>
2022-03-14 11:00:33 +0700

    add stats route

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,10 +6,11 @@ const {
    createJob,
    updateJob,
    deleteJob,
+   showStats,
 } = require("../controllers/jobsController");
 
 router.route("/").post(createJob).get(getAllJobs);
-router.route('/stats').get(showStats)
+router.route("/stats").get(showStats);
 router.route("/:id").patch(updateJob).delete(deleteJob);
 
 module.exports = router;
